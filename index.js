f07ea200504e83a88c9fbff4b8cd6cc147e206f0
Randy Carlson <randycarlson486@gmail.com>
2018-05-23 18:05:17 -0400

    down with magic numbers!

--- index.js
+++ index.js
@@ -2,15 +2,17 @@ const config = require('./config.json');
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+const chance = 0.3; // chance that bot will respond
+
 client.on('ready', () => {
     console.log('I am ready!');
 });
 
 client.on('message', message => {
-    console.log(message.content)
+    //console.log(message.content)
     var match = /^(?:i'm|i am)\s+([\w\s',-]+)/i.exec(message.content);
-    console.log(match)
-    if (match !== null && Math.random() < 0.5) {
+    //console.log(match)
+    if (match !== null && Math.random() < chance) {
         message.channel.send("Hi " + match[1] + ", I'm Dad!");
     }
 });
